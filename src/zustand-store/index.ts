1f738e7b4ca8340d57da1ca4860f6826d7e95248
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-06-20 10:54:29 -0300

    Do Redux ao Zustand - Setup do Zustand

---
+++ src/zustand-store/index.ts
@@ -0,0 +1,63 @@
+import { create } from "zustand";
+
+interface Course {
+  id: number
+  modules: Array<{
+    id: number
+    title: string
+    lessons: Array<{
+      id: string
+      title: string
+      duration: string
+    }>
+  }>
+}
+
+export interface PlayerState {
+  course: Course | null;
+  currentModuleIndex: number;
+  currentLessonIndex: number;
+  isLoading: boolean;
+
+  play: (moduleAndLessonIndex: [number, number]) => void;
+  next: () => void;
+}
+
+export const useStore = create<PlayerState>((set, get) => {
+  return {
+    course: null,
+    currentModuleIndex: 0,
+    currentLessonIndex: 0,
+    isLoading: false,
+
+    play: (moduleAndLessonIndex: [number, number]) => {
+      const [moduleIndex, lessonIndex] = moduleAndLessonIndex
+
+      set({
+        currentModuleIndex: moduleIndex,
+        currentLessonIndex: lessonIndex,
+      })
+    },
+
+    next: () => {
+      const { currentLessonIndex, currentModuleIndex, course } = get()
+
+      const nextLessonIndex = currentLessonIndex + 1;
+      const nextLesson = course?.modules[currentModuleIndex].lessons[nextLessonIndex];
+
+      if (nextLesson) {
+        set({ currentLessonIndex: nextLessonIndex })
+      } else {
+        const nextModuleIndex = currentModuleIndex + 1;
+        const nextModule = course?.modules[nextModuleIndex];
+
+        if (nextModule) {
+          set({
+            currentModuleIndex: nextModuleIndex,
+            currentLessonIndex: 0,
+          })
+        }
+      }
+    }
+  }
+})
